23b98b549ec757be691f0e0161acbe9b6fd5a9bc
Junghwan John Goh <jhgoh@cern.ch>
2013-12-04 19:03:34 +0900

    pat configuration

---
+++ Configuration/prod/ntuple/pat_ntuple_RD_cfg.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("PAT")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("Configuration.Geometry.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['com10']
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+process.source.fileNames = [
+#'/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7C-v2/10000/000560C1-FD97-E211-9F33-00304867924E.root'
+    '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',
+]
+
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string("out.root"),
+    outputCommands = cms.untracked.vstring(
+        'drop *',
+        'keep recoPFCandidates_particleFlow_*_*',
+        *patEventContentNoCleaning
+    )
+)
+process.outPath = cms.EndPath(process.out)
+
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+from PhysicsTools.PatAlgos.tools.pfTools import *
+
+runOnMC = False
+postfix = "PFlow"
+jetAlgo="AK5"
+#usePFBRECO(process,runPFBRECO=True, jetAlgo=jetAlgo, runOnMC=True, postfix=postfix)
+usePF2PAT(process, runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=runOnMC, postfix=postfix)
+
+process.p = cms.Path(
+#    #getattr(process,"patPFBRECOSequence"+postfix)
+    getattr(process,"patPF2PATSequence"+postfix)
+#  + process.patDefaultSequence
+)
+
+# top projections in PF2PAT:
+getattr(process,"pfNoPileUp"+postfix).enable = True
+getattr(process,"pfNoMuon"+postfix).enable = True
+getattr(process,"pfNoElectron"+postfix).enable = True
+getattr(process,"pfNoTau"+postfix).enable = False
+getattr(process,"pfNoJet"+postfix).enable = True
+
+# verbose flags for the PF2PAT modules
+getattr(process,"pfNoMuon"+postfix).verbose = False

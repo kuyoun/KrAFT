bf78100c6d169cf9065340cb551cd5eeb8463847
Junghwan John Goh <jhgoh@cern.ch>
2013-12-05 22:41:19 +0900

    cfg name change

--- Configuration/prod/ntuple/pat_ntuple_MC_cfg.py
+++ Configuration/prod/ntuple/pat_ntuple_MC_cfg.py
@@ -2,11 +2,12 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("PAT")
 runOnMC = True
 
-from KCMSAnalyses.Configuration.pat_template_cff import applyPAT
-applyPAT(process, runOnMC=runOnMC)
+from KCMSAnalyses.Configuration.customise_cff import customise
+customise(process, runOnMC=runOnMC)
 
 process.source.fileNames = [
     '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7C-v2/10000/000560C1-FD97-E211-9F33-00304867924E.root'
 ]
 
-process.maxEvents.input = 10
+process.maxEvents.input = 100
+
--- Configuration/prod/ntuple/pat_ntuple_RD_cfg.py
+++ Configuration/prod/ntuple/pat_ntuple_RD_cfg.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("PAT")
 runOnMC = False
 
-from KCMSAnalyses.Configuration.pat_template_cff import applyPAT
-applyPAT(process, runOnMC=runOnMC)
+from KCMSAnalyses.Configuration.customise_cff import customise
+customise(process, runOnMC=runOnMC)
 
 process.source.fileNames = [
     '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',

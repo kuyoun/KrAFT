a5a30bd87c44cf6b75feba73e3bfd65a33bbb420
Junghwan John Goh <jhgoh@cern.ch>
2013-12-05 17:14:08 +0900

    migration

--- LeptonAnalysis/python/leptonSelector_cfi.py
+++ LeptonAnalysis/python/leptonSelector_cfi.py
@@ -1,19 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-patMuonsWithRelIso = cms.EDFilter("PatRelIsoMuonSelector",
+goodMuons = cms.EDFilter("KCMuonSelector",
     rho = cms.InputTag("kt6PFJets", "rho"),
     src = cms.InputTag("patMuonsWithTrigger"),
+    precut = cms.string("pt > 20 && abs(eta) < 2.5"),
     cut = cms.string(""),
     minNumber = cms.uint32(0),
     maxNumber = cms.uint32(999),
     coneSize = cms.double(0.3),
 )
 
-patElectronsWithRelIso = cms.EDFilter("PatRelIsoElectronSelector",
+goodElectrons = cms.EDFilter("KCElectronSelector",
     rho = cms.InputTag("kt6PFJets", "rho"),
     src = cms.InputTag("patElectronsWithTrigger"),
+    precut = cms.string("pt > 20 && abs(eta) < 2.5"),
     cut = cms.string(""),
     minNumber = cms.uint32(0),
     maxNumber = cms.uint32(999),
     coneSize = cms.double(0.3),
 )
+

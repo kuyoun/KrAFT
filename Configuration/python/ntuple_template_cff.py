7d44051b80d3701e0c9b6b7e83e149c450c2166b
Junghwan John Goh <jhgoh@cern.ch>
2013-12-07 12:10:13 +0900

    separate cfi file

--- Configuration/python/ntuple_template_cff.py
+++ Configuration/python/ntuple_template_cff.py
@@ -18,42 +18,19 @@ TFileService = cms.Service("TFileService",
     fileName = cms.string("ntuple.root"),
 )
 
-event = cms.EDAnalyzer("KFlatTreeMaker",
-    isMC = cms.bool(False),
+from KCMSAnalyses.FlatTree.flatTreeMaker_cfi import *
 
-    gen = cms.InputTag("genParticles"),
-    genJet = cms.InputTag("ak5GenJets"),
-    recoToGenJetMap = cms.InputTag("recoToGenJetMap"),
-    genJetToPartonsMap = cms.InputTag("genJetToPartonsMap"),
-
-    weight = cms.string("pileupWeight"),
-    vertex = cms.InputTag("goodOfflinePrimaryVertices"),
-    eventCounters = cms.vstring(
-    ),
-
-    electron = cms.PSet(
-        src = cms.InputTag("goodElectrons"),
-        minNumber = cms.uint32(0),
-        maxNumber = cms.uint32(999),
-    ),
-    muon = cms.PSet(
-        src = cms.InputTag("goodMuons"),
-        minNumber = cms.uint32(0),
-        maxNumber = cms.uint32(999),
-    ),
-    jet = cms.PSet(
-        src = cms.string("goodJets"),
-        leptonDeltaR = cms.double(0.5),
-        bTagType = cms.string("combinedSecondaryVertexBJetTags"),
-    ),
-    met = cms.PSet(
-        src = cms.string("goodJets"),
-    ),
-)
+MuMu = event.clone()
+ElEl = event.clone()
+MuEl = event.clone()
+MuMu.muon.minNumber = 2
+ElEl.electron.minNumber = 2
+MuEl.muon.minNumber = 1
+MuEl.electron.minNumber = 1
 
 ntupleSequence = cms.Sequence(
     goodOfflinePrimaryVertices * pileupWeight
-  + goodMuons + goodElectrons 
+  + goodMuons + goodElectrons
   * goodJets
-  * event
+  * MuMu + ElEl + MuEl
 )
---
+++ FlatTree/python/flatTreeMaker_cfi.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+event = cms.EDAnalyzer("KFlatTreeMaker",
+    isMC = cms.bool(False),
+
+    gen = cms.InputTag("genParticles"),
+    genJet = cms.InputTag("ak5GenJets"),
+    recoToGenJetMap = cms.InputTag("recoToGenJetMap"),
+    genJetToPartonsMap = cms.InputTag("genJetToPartonsMap"),
+
+    weight = cms.string("pileupWeight"),
+    vertex = cms.InputTag("goodOfflinePrimaryVertices"),
+    eventCounters = cms.vstring(
+    ),
+
+    electron = cms.PSet(
+        src = cms.InputTag("goodElectrons"),
+        minNumber = cms.uint32(0),
+        maxNumber = cms.uint32(999),
+    ),
+    muon = cms.PSet(
+        src = cms.InputTag("goodMuons"),
+        minNumber = cms.uint32(0),
+        maxNumber = cms.uint32(999),
+    ),
+    jet = cms.PSet(
+        src = cms.string("goodJets"),
+        leptonDeltaR = cms.double(0.5),
+        bTagType = cms.string("combinedSecondaryVertexBJetTags"),
+    ),
+    met = cms.PSet(
+        src = cms.string("goodJets"),
+    ),
+)

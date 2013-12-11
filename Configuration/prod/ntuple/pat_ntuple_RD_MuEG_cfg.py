8c0d3801b83b1faa33ca7c559a745ef93c75e876
Junghwan John Goh <jhgoh@cern.ch>
2013-12-11 15:22:51 +0900

    separate cfg files

---
+++ Configuration/prod/ntuple/pat_ntuple_RD_MuEG_cfg.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+runOnMC = False
+
+from KCMSAnalyses.Configuration.customise_cff import *
+process = initialise(decayMode="dilepton", runOnMC=runOnMC)
+addNtupleStep(process, runOnMC=runOnMC)
+
+process.source.fileNames = [
+    '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',
+]
+

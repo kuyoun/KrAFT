14377e7a7f8ab2ccb8af9e742be059cb9313bc46
Junghwan John Goh <jhgoh@cern.ch>
2014-01-21 18:06:43 +0100

    hostname based sample selection

--- Configuration/prod/ntuple/pat_ntuple_MC_Dilepton_cfg.py
+++ Configuration/prod/ntuple/pat_ntuple_MC_Dilepton_cfg.py
@@ -5,9 +5,17 @@ from KrAFT.Configuration.customise_cff import *
 process = initialise(decayMode="dilepton", runOnMC=runOnMC)
 addNtupleStep(process, runOnMC=runOnMC)
 
-process.source.fileNames = [
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7C-v2/10000/000560C1-FD97-E211-9F33-00304867924E.root'
+import os
+hostName = os.environ['HOSTNAME']
+if 'cern.ch' in hostName:
+    process.source.fileNames = [
+        '/store/relval//CMSSW_5_3_12_patch2/RelValProdTTbar/GEN-SIM-RECO/START53_LV2-v1/00000/5E865D62-AA2B-E311-AA04-002618943962.root',
+        '/store/relval//CMSSW_5_3_12_patch2/RelValProdTTbar/GEN-SIM-RECO/START53_LV2-v1/00000/92EB24DF-C72B-E311-8AA2-00261894390E.root',
+    ]
+elif 'uos.ac.kr' in hostName
+    process.source.fileNames = [
+        '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7C-v2/10000/000560C1-FD97-E211-9F33-00304867924E.root'
 ]
 
-#process.maxEvents.input = 100
+process.maxEvents.input = 100
 

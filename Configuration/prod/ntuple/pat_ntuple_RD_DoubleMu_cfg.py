080262b3c3c8fd4722280df71f07e116409922c9
Junghwan John Goh <jhgoh@cern.ch>
2014-01-21 18:10:06 +0100

    host based samples

--- Configuration/prod/ntuple/pat_ntuple_RD_DoubleMu_cfg.py
+++ Configuration/prod/ntuple/pat_ntuple_RD_DoubleMu_cfg.py
@@ -5,7 +5,16 @@ from KrAFT.Configuration.customise_cff import *
 process = initialise(decayMode="MuMu", runOnMC=runOnMC)
 addNtupleStep(process, runOnMC=runOnMC)
 
-process.source.fileNames = [
-    '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',
-]
+import os
+hostName = os.environ['HOSTNAME']
+if 'cern.ch' in hostName:
+    process.source.fileNames = [
+        '/store/data/Run2012D/DoubleElectron/AOD/22Jan2013-v1/10000/FEED5E9F-6A8F-E211-91C4-00261894391C.root',
+    ]
+elif 'uos.ac.kr' in hostName
+    process.source.fileNames = [
+        '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',
+    ]
+
+process.maxEvents.input = 100
 

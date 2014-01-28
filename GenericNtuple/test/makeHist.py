c49b125ec3762eb5ddec274c4443b11fc64aaae0
Junghwan John Goh <jhgoh@cern.ch>
2014-01-28 13:28:07 +0900

    added lb system mass and data driven QCD

--- GenericNtuple/test/makeHist.py
+++ GenericNtuple/test/makeHist.py
@@ -26,6 +26,10 @@ def process(sample, mode, files):
         cut_s1 += "&& decayMode < 1"
     elif "TTJets" in sample or "TTTo" in sample or "TT_" in sample:
         cut_s1 += "&& decayMode >= 1"
+    elif "QCD" == sample:
+        #cut_s1 = "z_m > 12 && lepton1_pt > 20 && lepton2_pt > 20 && lepton1_iso > 0.15 && lepton2_iso > 0.15 && z_Q != 0"
+        cut_s1 = "z_m > 12 && lepton1_pt > 20 && lepton2_pt > 20 && lepton1_iso > 0.15 && z_Q != 0"
+        cut_s4 = "1"
 
     ana.addH1("zM", "z_m", "MZ;M(l^{+}l^{-}) (GeV/c^{2});Events per 2 GeV/c^{2}", 100, 0, 200)
     ana.addH1("lepton1_pt", "lepton1_pt", "pt1;Leading lepton p_{T} (GeV/c);Events per 2GeV/c", 100, 0, 200)
@@ -34,12 +38,15 @@ def process(sample, mode, files):
     ana.addH1("nbjet", "bjets_n", "nbjet;B jet multiplicity;Events", 10, 0, 10)
     ana.addH1("met", "met_pt", "met;Missing transverse momentum p_{T} (GeV/c);Events per 2GeV/c", 100, 0, 200)
     ana.addH1("nVertex", "nVertex", "nVertex;Vertex multiplicity;Events", 60, 0, 60)
+    ana.addH1("vsumM", "ttbar_vsumM", "vsumM;M(l^{+}l^{-}j_{1}j_{2}MET) (GeV/c^{2});Events per 20GeV/c^{2}", 100, 0, 2000)
+    ana.addH1("lb1M", "lb1_m", "lb1M;M(l_{1}^{#pm}b) (GeV/c^{2});Events per 5GeV/c^{2}", 100, 0, 500)
+    ana.addH1("lb2M", "lb2_m", "lb2M;M(l_{2}^{#pm}b) (GeV/c^{2});Events per 5GeV/c^{2}", 100, 0, 500)
 
     ana.addCutStep("S1", cut_s1, "zM,lepton1_pt,lepton2_pt,nVertex")
     ana.addCutStep("S2", cut_s2, "zM,njet,nbjet,met,nVertex")
     ana.addCutStep("S3", cut_s3, "njet,nbjet,met,nVertex")
-    ana.addCutStep("S4", cut_s4, "nbjet,met,nVertex")
-    ana.addCutStep("S5", cut_s5, "nbjet,met,nVertex")
+    ana.addCutStep("S4", cut_s4, "nbjet,met,nVertex,vsumM,lb1M,lb2M")
+    ana.addCutStep("S5", cut_s5, "nbjet,met,nVertex,vsumM,lb1M,lb2M")
 
     ana.process()
 
@@ -55,6 +62,9 @@ if __name__ == '__main__':
             sampleName = ('_'.join(s.split('_')[:-1]), mode)
             if sampleName not in samples: samples[sampleName] = []
             samples[sampleName].append("ntuple/%s" % f)
+    samples[("QCD", "MuMu")] = ["ntuple/DoubleMu_Run2012%s-22Jan2013_0__MuMu.root" % x for x in "ABCD"]
+    samples[("QCD", "ElEl")] = ["ntuple/DoubleElectron_Run2012%s-22Jan2013_0__ElEl.root" % x for x in "ABCD"]
+    samples[("QCD", "MuEl")] = ["ntuple/MuEG_Run2012%s-22Jan2013_0__MuEl.root" % x for x in "ABCD"]
 
     p = multiprocessing.Pool(multiprocessing.cpu_count())
     for key in samples.keys():

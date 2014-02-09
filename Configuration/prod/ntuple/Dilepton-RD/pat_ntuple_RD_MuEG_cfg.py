0d469a4726423a27b73e5b6be1d31b3b0b98bf99
Junghwan John Goh <jhgoh@cern.ch>
2014-02-09 23:06:59 +0900

    typo

--- Configuration/prod/ntuple/Dilepton-RD/pat_ntuple_RD_MuEG_cfg.py
+++ Configuration/prod/ntuple/Dilepton-RD/pat_ntuple_RD_MuEG_cfg.py
@@ -11,7 +11,7 @@ if 'cern.ch' in hostName:
     process.source.fileNames = [
         '/store/data/Run2012D/DoubleElectron/AOD/22Jan2013-v1/10000/FEED5E9F-6A8F-E211-91C4-00261894391C.root',
     ]
-elif 'uos.ac.kr' in hostName
+elif 'uos.ac.kr' in hostName:
     process.source.fileNames = [
         '/store/user/jhgoh/MuEG/Run2012A-22Jan2013-v1-KCMSSkim20131027_1/4407ef23eed415918ae815f01ecb7627/skim_1_1_2W6.root',
     ]

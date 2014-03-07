4e0742d83aeedf290cabae853d5058cda2f6709a
Junghwan John Goh <jhgoh@cern.ch>
2014-03-07 20:03:27 +0100

    use PV

--- RecoSelectorTools/python/leptonSelector_cfi.py
+++ RecoSelectorTools/python/leptonSelector_cfi.py
@@ -3,8 +3,14 @@ import FWCore.ParameterSet.Config as cms
 goodMuons = cms.EDFilter("KMuonSelector",
     rho = cms.InputTag("kt6PFJets", "rho"),
     src = cms.InputTag("patMuonsPFlow"),
+
     precut = cms.string("pt > 20 && abs(eta) < 2.5"),
+
     cut = cms.string(""),
+    vertex = cms.InputTag("goodOfflinePrimaryVertices"),
+    maxDz = cms.double(1e9), ## 0.5 for tight muons
+    maxDxy = cms.double(1e9), ## 0.2 for tight muons
+
     minNumber = cms.uint32(0),
     maxNumber = cms.uint32(999),
     coneSize = cms.double(0.3),
@@ -13,8 +19,14 @@ goodMuons = cms.EDFilter("KMuonSelector",
 goodElectrons = cms.EDFilter("KElectronSelector",
     rho = cms.InputTag("kt6PFJets", "rho"),
     src = cms.InputTag("patElectronsPFlow"),
+
     precut = cms.string("pt > 20 && abs(eta) < 2.5"),
+
     cut = cms.string(""),
+    vertex = cms.InputTag("goodOfflinePrimaryVertices"),
+    maxDz = cms.double(1e9), ## for cut based WP: 0.2/0.2/0.1/0.1 for veto/loose/medium/tight electrons
+    maxDxy = cms.double(1e9), ## for cut based WP: 0.04/0.02/0.02/0.02 for veto/loose/medium/tight electrons
+
     minNumber = cms.uint32(0),
     maxNumber = cms.uint32(999),
     coneSize = cms.double(0.3),

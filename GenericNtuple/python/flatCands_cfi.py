e668627ab16711be24ef3430c03d25e384eaec8f
Junghwan John Goh <jhgoh@cern.ch>
2014-07-18 09:05:37 +0900

    store variables

--- GenericNtuple/python/flatCands_cfi.py
+++ GenericNtuple/python/flatCands_cfi.py
@@ -15,3 +15,32 @@ flatJetCands = cms.EDProducer("FlatCandProducer",
     src = cms.InputTag("selectedJetsPFlow"),
 )
 
+flatCandNtuple = cms.EDAnalyzer("FlatCandToNtupleMaker",
+    srcs = cms.VPSet(
+        cms.PSet(
+            src = cms.InputTag("flatMuonCands"),
+            vmaps = cms.VInputTag(
+                cms.InputTag("flatMuonCands", "isTight"),
+                cms.InputTag("flatMuonCands", "isLoose"),
+                cms.InputTag("flatMuonCands", "relIsoDbeta03"),
+                cms.InputTag("flatMuonCands", "relIsoDbeta04"),
+            ),
+        ),
+        cms.PSet(
+            src = cms.InputTag("flatElectronCands"),
+            vmaps = cms.VInputTag(
+                cms.InputTag("flatElectronCands", "mva"),
+                cms.InputTag("flatElectronCands", "relIsoDbeta03"),
+                cms.InputTag("flatElectronCands", "relIsoDbeta04"),
+                cms.InputTag("flatElectronCands", "relIsoRho03"),
+                cms.InputTag("flatElectronCands", "relIsoRho04"),
+            ),
+        ),
+        cms.PSet(
+            src = cms.InputTag("flatJetCands"),
+            vmaps = cms.VInputTag(
+                cms.InputTag("flatJetCands", "bTagCSV"),
+            ),
+        ),
+    ),
+)

7ab7c54bb89063a00da5ac9217cc6b5a8f74b78c
Junghwan John Goh <jhgoh@cern.ch>
2014-08-08 12:30:02 +0200

    produce HLT info

--- GenericNtuple/python/flatEventInfo_cfi.py
+++ GenericNtuple/python/flatEventInfo_cfi.py
@@ -3,6 +3,11 @@ import FWCore.ParameterSet.Config as cms
 flatEventInfo = cms.EDProducer("FlatEventInfoProducer",
     genInfo = cms.InputTag("generator"),
     vertex = cms.InputTag("goodOfflinePrimaryVertices"),
-    hltPaths = cms.vstring(),
+    hltProcessName = cms.string("HLT"),
+    HLT = cms.PSet(
+        DoubleMu = cms.vstring(),
+        DoubleElectron = cms.vstring(),
+        MuEG = cms.vstring(),
+    ),
 )
 

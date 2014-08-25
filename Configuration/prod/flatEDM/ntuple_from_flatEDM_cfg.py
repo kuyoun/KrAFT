bdc5ec9c4bdd5d266e32abeb077ba49f1e756992
Geonmo <ry840901@gmail.com>
2014-08-25 20:26:21 +0900

    Fix missing )

--- Configuration/prod/flatEDM/ntuple_from_flatEDM_cfg.py
+++ Configuration/prod/flatEDM/ntuple_from_flatEDM_cfg.py
@@ -25,7 +25,7 @@ process.p = cms.Path(process.fEvent)
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
-
+)
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string("ntuple.root"),
 )
